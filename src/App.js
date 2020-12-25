50c6f4c86d376b25f241b47e5cd4b46b3ea70566
NeverSettels <settels.eric@gmail.com>
2020-12-25 11:22:13 -0800

    added basic button oress function to begining of prgoram

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-
+import React, { useState } from 'react'
+import Puzzle from './Components/Puzzle/Puzzle'
+import Info from './Components/Info/Info'
+import Start from "./Components/Start"
 import './App.css';
 
 function App() {
+  const [view, setview] = useState("start")
+  changeView = view =>{
+    view=="puzzle"?setview("puzzle"):setview("info") 
+  }
   return (
     <>
-    <div className="puzzle">
-
-    </div>
-    <div className="portafolio"></div>
+     {view=='start'? <Start/>:""}
+     {view=='puzzle'? <Puzzle/>:""}
+     {view=='info'? <Info/>:""}
       </>
   );
 }
